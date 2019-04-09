1c5ca9b2b9897c9241be7acca9a5bd7edbf9f8e6
Ginger <>
2019-04-09 10:06:25 +0200

    cambios por Ginger

--- noestabien.sql
+++ noestabien.sql
@@ -3,8 +3,8 @@ create table TABLAENDE
   num         number(5),
   nom         varchar2(30),
   dir         varchar2(50),
-  fecha_nac   date
-  CONSTRAINT cp_tablaende PRIMARY KEY (num)
+  fecha_nac   date,
+  CONSTRAINT cp_TABLAENDE PRIMARY KEY (num)
  )
  /
  
